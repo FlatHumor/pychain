12c96e448bca042da06e9921675293b9bc9efc55
Markel Sudnev <m.sudnev@gmail.com>
2019-01-30 09:55:29 +0300

    Added cli arguments

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 
 import os
+import sys
 from transaction import Transaction
 from brick import Brick
 from filerepository import FileRepository
@@ -7,9 +8,17 @@ from chain import Chain
 
 
 if __name__ == '__main__':
-    chain_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "chain")
+    chain_path = os.path.join(os.path.split(os.path.abspath(__file__))[0], "chain") 
+    sender = "example sender"
+    receiver = "example receiver"
+    content = "example content"
+    if len(sys.argv) == 5:
+        chain_path = sys.argv[1]
+        sender = sys.argv[2]
+        receiver = sys.argv[3]
+        content = sys.argv[4]
     repo = FileRepository(chain_path)
-    transaction = Transaction("sender", "receiver", "transaction content")
+    transaction = Transaction(sender, receiver, content)
     chain = Chain()
     chain.link_repository(repo)
     if chain.is_valid():
